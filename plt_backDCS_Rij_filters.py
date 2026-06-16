7acf5e1676b0ad34ac5949844b7eb704aafc989b
Lily <lilyr8743@gmail.com>
2026-06-16 15:14:16 -0400

    formatting

--- plt_backDCS_Rij_filters.py
+++ plt_backDCS_Rij_filters.py
@@ -7,6 +7,7 @@ plt.rc('legend', fontsize=14)
 plt.rc('xtick', labelsize=14)
 plt.rc('ytick', labelsize=14)
 
+
 def load_plot_data(path):
     data = np.loadtxt(path)
     if data.ndim == 1:
@@ -22,8 +23,9 @@ vertonal_lt4 = load_plot_data('./vertonal/vertonal_IAM_Rij_lt4_0.41.dat')
 axs[0].plot(camphor_lt4[:, 0], camphor_lt4[:, 2], label='camphor')
 axs[0].plot(fenchone_lt4[:, 0], fenchone_lt4[:, 2], label='fenchone')
 axs[0].plot(vertonal_lt4[:, 0], vertonal_lt4[:, 2], label='vertonal')
-axs[0].set_title("Rij < 4")
-axs[0].set_xlabel('Energy (eV)')
+axs[0].text(0.02, 0.95, 'Rij < 4', transform=axs[0].transAxes, va='top', ha='left', fontsize=16,
+            bbox=dict(facecolor='white', edgecolor='black', boxstyle='square,pad=0.3'))
+#axs[0].set_xlabel('Energy (eV)')
 axs[0].set_ylabel('DCS')
 axs[0].set_xlim(50, 100)
 axs[0].legend(loc='upper right')
@@ -34,8 +36,9 @@ vertonal_lt6 = load_plot_data('./vertonal/vertonal_IAM_Rij_lt6_0.41.dat')
 axs[1].plot(camphor_lt6[:, 0], camphor_lt6[:, 2], label='camphor')
 axs[1].plot(fenchone_lt6[:, 0], fenchone_lt6[:, 2], label='fenchone')
 axs[1].plot(vertonal_lt6[:, 0], vertonal_lt6[:, 2], label='vertonal')
-axs[1].set_title("Rij < 6")
-axs[1].set_xlabel('Energy (eV)')
+axs[1].text(0.02, 0.95, 'Rij < 6', transform=axs[1].transAxes, va='top', ha='left', fontsize=16,
+            bbox=dict(facecolor='white', edgecolor='black', boxstyle='square,pad=0.3'))
+#axs[1].set_xlabel('Energy (eV)')
 axs[1].set_ylabel('DCS')
 
 camphor_lt8 = load_plot_data('./camphor/camphor_IAM_Rij_lt8_0.41.dat')
@@ -44,7 +47,8 @@ vertonal_lt8 = load_plot_data('./vertonal/vertonal_IAM_Rij_lt8_0.41.dat')
 axs[2].plot(camphor_lt8[:, 0], camphor_lt8[:, 2], label='camphor')
 axs[2].plot(fenchone_lt8[:, 0], fenchone_lt8[:, 2], label='fenchone')
 axs[2].plot(vertonal_lt8[:, 0], vertonal_lt8[:, 2], label='vertonal')
-axs[2].set_title("Rij < 8")
+axs[2].text(0.02, 0.95, 'Rij < 8', transform=axs[2].transAxes, va='top', ha='left', fontsize=16,
+            bbox=dict(facecolor='white', edgecolor='black', boxstyle='square,pad=0.3'))
 axs[2].set_xlabel('Energy (eV)')
 axs[2].set_ylabel('DCS')
 
