ee1c17b5514e48226aa6700eb844514c0e9e9e8b
Lily Richard <lilyr8743@gmail.com>
2026-06-30 13:07:53 -0400

    commented out vertonal

--- plt_combined_Back_DCS.py
+++ plt_combined_Back_DCS.py
@@ -14,34 +14,34 @@ mpl.rcParams['legend.fontsize'] = 14
 
 camphor_file = "/Users/lilyrichard/Library/CloudStorage/OneDrive-KennesawStateUniversity/Research/DAMOP_2026/Results/Backscattered_DCS/camphor_cat_BackscatteredDCS.csv"
 fenchone_file = "/Users/lilyrichard/Library/CloudStorage/OneDrive-KennesawStateUniversity/Research/DAMOP_2026/Results/Backscattered_DCS/fenchone_cat_BackscatteredDCS.csv" 
-vertonal_file = "/Users/lilyrichard/Library/CloudStorage/OneDrive-KennesawStateUniversity/Research/DAMOP_2026/Results/Backscattered_DCS/vertonal_cat_BackscatteredDCS.csv"
+# vertonal_file = "/Users/lilyrichard/Library/CloudStorage/OneDrive-KennesawStateUniversity/Research/DAMOP_2026/Results/Backscattered_DCS/vertonal_cat_BackscatteredDCS.csv"
 
 
 camphor = np.loadtxt(camphor_file, delimiter=",", skiprows = 1)
 fenchone = np.loadtxt(fenchone_file, delimiter=",", skiprows = 1)
-vertonal = np.loadtxt(vertonal_file, delimiter=",", skiprows = 1)
+# vertonal = np.loadtxt(vertonal_file, delimiter=",", skiprows = 1)
 
 Energy = camphor[:, 0]
 
 camphor_IAM=camphor[:, 2]
 fenchone_IAM=fenchone[:, 2]
-vertonal_IAM=vertonal[:, 2]
+# vertonal_IAM=vertonal[:, 2]
 
 camphor_TIIAM= camphor[:, 1]
 fenchone_TIIAM= fenchone[:, 1]
-vertonal_TIIAM= vertonal[:, 1]
+# vertonal_TIIAM= vertonal[:, 1]
 
-ICF_TIIAM_CV = (camphor_TIIAM - vertonal_TIIAM) / (camphor_TIIAM + vertonal_TIIAM)
-ICF_TIIAM_FV = (fenchone_TIIAM - vertonal_TIIAM) / (fenchone_TIIAM + vertonal_TIIAM)
+# ICF_TIIAM_CV = (camphor_TIIAM - vertonal_TIIAM) / (camphor_TIIAM + vertonal_TIIAM)
+# ICF_TIIAM_FV = (fenchone_TIIAM - vertonal_TIIAM) / (fenchone_TIIAM + vertonal_TIIAM)
 
-ICF_IAM_CV = (camphor_IAM - vertonal_IAM) / (camphor_IAM + vertonal_IAM)
-ICF_IAM_FV = (fenchone_IAM - vertonal_IAM) / (fenchone_IAM + vertonal_IAM)
+# ICF_IAM_CV = (camphor_IAM - vertonal_IAM) / (camphor_IAM + vertonal_IAM)
+# ICF_IAM_FV = (fenchone_IAM - vertonal_IAM) / (fenchone_IAM + vertonal_IAM)
 
 
 ##################DCS plots##########################
 plt.plot(Energy, camphor_IAM, label = "Camphor")
 plt.plot(Energy, fenchone_IAM, label = "Fenchone")
-plt.plot(Energy, vertonal_IAM, label = "Vertonal")
+# plt.plot(Energy, vertonal_IAM, label = "Vertonal")
 plt.title("IAM Backscattered DCS")
 plt.xlabel("Energy (eV)")
 plt.ylabel("DCS")
@@ -50,7 +50,7 @@ plt.show()
 
 plt.plot(Energy, camphor_TIIAM, label = "Camphor")
 plt.plot(Energy, fenchone_TIIAM, label = "Fenchone")
-plt.plot(Energy, vertonal_TIIAM, label = "Vertonal")
+# plt.plot(Energy, vertonal_TIIAM, label = "Vertonal")
 plt.title("TI-IAM Backscattered DCS")
 plt.xlabel("Energy (eV)")
 plt.ylabel("DCS")
@@ -66,8 +66,8 @@ bottom_margin = 0.14
 
 fig1 = plt.figure()
 
-plt.plot(Energy, ICF_TIIAM_CV, label='TI-IAM (Camphor–Vertonal)')
-plt.plot(Energy, ICF_IAM_CV, label='IAM (Camphor–Vertonal)')
+# plt.plot(Energy, ICF_TIIAM_CV, label='TI-IAM (Camphor–Vertonal)')
+# plt.plot(Energy, ICF_IAM_CV, label='IAM (Camphor–Vertonal)')
 
 plt.xlabel("Energy (eV)")
 plt.ylabel("ICF")
@@ -81,8 +81,8 @@ plt.show()
 
 fig2 = plt.figure()
 
-plt.plot(Energy, ICF_TIIAM_FV, label='TI-IAM (Fenchone–Vertonal)')
-plt.plot(Energy, ICF_IAM_FV, label='IAM (Fenchone–Vertonal)')
+# plt.plot(Energy, ICF_TIIAM_FV, label='TI-IAM (Fenchone–Vertonal)')
+# plt.plot(Energy, ICF_IAM_FV, label='IAM (Fenchone–Vertonal)')
 
 plt.xlabel("Energy (eV)")
 plt.ylabel("ICF")
